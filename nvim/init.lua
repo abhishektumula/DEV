39bf11430df28698cd1bada8e23e52225de2c846
abhishektumula <abhishektumula@gmail.com>
2025-08-27 08:35:31 +0530

    feat: may be good bye to Ubuntu...

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,10 +6,41 @@ vim.opt.shiftwidth = 2
 vim.opt.background = "dark" -- make sure the theme is in dark mode
 vim.opt.cmdheight = 1 
 vim.opt.colorcolumn = "80"
+vim.opt.showmode = true
+-- important
 vim.cmd.colorscheme("rose-pine")
+-- important
 vim.opt.scrolloff = 7
+-- Make all keywords bold
+vim.cmd("highlight Keyword gui=bold cterm=bold")
+-- Make all functions bold
+vim.cmd("highlight Function gui=bold cterm=bold")
+-- (Optional) Make types, constants, and identifiers bold too
+vim.cmd("highlight Type gui=bold cterm=bold")
+vim.cmd("highlight Constant gui=bold cterm=bold")
+vim.cmd("highlight Identifier gui=bold cterm=bold")
+vim.opt.guicursor = "a:block"
 
---vim.opt.guicursor = "a:block"
+-- Auto-remove extra blank lines at EOF, keep max 9
+
+-- Ensure exactly 9 blank lines at EOF
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[
+      keepjumps keeppatterns %s/\(\n\)\{1,}\%$//e
+      for i in range(9)
+        $put =''
+      endfor
+    ]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
+
+
+
+
 --Universal transparency overrides
 local transparent_groups = {
   "Normal",
--- nvim/lua/plugins/disable.lua
+++ nvim/lua/plugins/disable.lua
@@ -1,5 +1,26 @@
+
 return {
-    {"goolord/alpha-nvim", enabled = false},
-    { "lukas-reineke/indent-blankline.nvim", enabled = false },
-    { "echasnovski/mini.indentscope", enabled = false },
+  -- Disable the dashboard
+  { "goolord/alpha-nvim", enabled = false },
+
+  -- Disable indent guides
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+
+  -- Optional: disable LazyVim's Snacks dashboard too
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = { enabled = false },
+    },
+  },
 }
+
+
+
+
+
+
+
+
+
